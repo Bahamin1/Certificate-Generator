5821c16ec01d842baae89ecb48f1d5a02c8e9df4
Bahamin Dehpour <bahamindehpour@gmail.com>
2024-12-20 20:38:12 +0330

    Api and verify update

---
+++ pages/api/hello.js
@@ -0,0 +1,10 @@
+// pages/api/hello.js
+
+export default function handler(req, res) {
+    if (req.method === 'GET') {
+      res.status(200).json({ message: 'Hello from server!' })
+    } else {
+      res.status(405).json({ error: 'Method Not Allowed' })
+    }
+  }
+  
